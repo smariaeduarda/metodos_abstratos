12a0db5b2d0cf16dd472dce1d3ed7fde82903ab9
Maria Eduarda <mariaeduardasilv916@gmail.com>
2023-05-10 12:07:12 -0300

    Baixando versão

---
+++ src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<TaxPayer> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of tax payers: ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println("Tax payer #" + i + " data:");
+			System.out.print("Individual or Company (i/c)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Anual Income: ");
+			double anualIncome = sc.nextDouble();
+			if(ch == 'i') {
+				System.out.print("Health expenditures: ");
+				double healthExenditures = sc.nextDouble();
+				list.add(new Individual(name, anualIncome, healthExenditures));
+			}
+			else {
+				System.out.print("Number of employees: ");
+				int numberOfEmployees = sc.nextInt();
+				list.add(new Company(name, anualIncome, numberOfEmployees));
+			}
+		}
+		
+		System.out.println();
+		System.out.println("TAXES PAID:");
+		for (TaxPayer taxPayer : list) {
+			System.out.println(taxPayer.getName() + ": $ " + String.format( "%.2f", taxPayer.tax()));
+		}
+		
+		
+		sc.close();
+	}
+
+}
